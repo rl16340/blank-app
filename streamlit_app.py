bb71bcbee5f895b7ee7ab29e8fcfcff5dd1cba4c
rl16340 <rl16340@gmail.com>
2025-08-17 06:32:55 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
+import pandas as pd
 
-st.title("🎈 My new app")
+
+st.title("🎈 Streamlit ")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
